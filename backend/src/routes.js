7f2540e19061f0a572eae7cc8c2870ca53c2f007
Giancarlo <giandroberto@yahoo.com.br>
2020-03-28 14:25:33 -0300

    Add endpoint to create NGO

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection')
 
 const routes = express.Router();
 
@@ -6,6 +8,22 @@ routes.get('/', (_request, response) => {
   return response.json({
     message: 'Hello world!'
   });
+});
+
+routes.post('/ngos', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ngos').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
