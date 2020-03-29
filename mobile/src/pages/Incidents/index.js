08bf630d047af226fb78cda97519cf568871a3ef
Giancarlo <giandroberto@yahoo.com.br>
2020-03-29 16:06:49 -0300

    Implement infinite scroll

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,6 +12,9 @@ import styles from './styles';
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
 
   function navigateToDetail(incident) {
@@ -19,11 +22,25 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const response = await api.get('incidents');
+    if (loading) {
+      return;
+    }
+
+    if (total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('incidents', {
+      params: { page }
+    });
 
     try {
-      setIncidents(response.data);
+      setIncidents([...incidents, ...response.data]);
       setTotal(response.headers['x-total-count']);
+      setPage(page + 1);
+      setLoading(false);
     } catch (err) {
       //no
     }
@@ -49,7 +66,9 @@ export default function Incidents() {
         style={styles.incidentList}
         data={incidents}
         keyExtractor={incident => String(incident.id)}
-        showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG: </Text>
